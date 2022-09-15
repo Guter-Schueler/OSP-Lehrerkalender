84ec173946ab19ba4ff05acc39c64b279f1c0142
Syvex <oliverwiese@hotmail.de>
2022-09-15 13:35:51 +0200

    re-init react outside of stackblitz

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src="https://www.svgrepo.com/show/7076/monthly-calendar.svg" className="App-logo" alt="logo" />
+        <span
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          OSP Lehrerkalender
+        </span>
       </header>
     </div>
   );
