e8950035309cc93c1ae769a70dc93f393feaad7e
Dogus Oezkan <dogus-talha.oezkan@avenga.com>
2022-11-07 11:30:17 +0100

    fixed everything hopefully

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://www.svgrepo.com/show/7076/monthly-calendar.svg"
+          className="App-logo"
+          alt="logo"
+        />
+        <span className="App-link">OSP Lehrerkalender</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
