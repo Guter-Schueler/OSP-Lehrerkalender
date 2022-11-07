8589d4dc9ed72b095c3a1cb1c6680a3c3391acbb
Dogus Oezkan <dogus-talha.oezkan@avenga.com>
2022-11-07 11:53:10 +0100

    add login

---
+++ frontend/src/login.jsx
@@ -0,0 +1,23 @@
+import userStore from './store.js';
+import './login.scss';
+
+export default function Login() {
+  const { login, loginError } = userStore();
+
+  return (
+    <div className="loginWrapper">
+      {loginError && (
+        <div className="errorBox">
+          <p>{loginError}</p>
+        </div>
+      )}
+      <form onSubmit={login}>
+        <label htmlFor="userName">Username</label>
+        <input id="userName" type="text"></input>
+        <label htmlFor="password">Passwort</label>
+        <input id="password" type="password"></input>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
