67b1410e8df5579cce902507eb4ed06c6eea0a9e
Cihan <cihan.kurt@avenga.com>
2022-11-09 12:25:08 +0100

    further fixes in backend

--- backend.js
+++ backend.js
@@ -259,18 +259,6 @@ app.post('/schuelerKlasseRef', checkToken, (req, res) => {
   );
 });
 
-// Wann wird in klassen was geposted und wer darf das ? dementsprechend muss hier angepasst werden // kein gültig bis?
-
-app.get('/unterricht', checkToken, (req, res) => {
-  const response = db.all(
-    'SELECT id, kuerzel, vorname, nachname, passwort FROM lehrer',
-    function (err, rows) {
-      res.send(rows);
-    }
-  );
-  return response;
-});
-
 app.get('/unterricht', checkToken, (req, res) => {
   const response = db.all(
     'SELECT lehrerId, klassenId, faecherId FROM unterricht',
@@ -279,29 +267,6 @@ app.get('/unterricht', checkToken, (req, res) => {
     }
   );
   return response;
-});
-
-// Wann wird in klassen was geposted und wer darf das ? dementsprechend muss hier angepasst werden // kein gültig bis?
-app.post('/unterricht', checkToken, (req, res) => {
-  db.get(
-    'INSERT INTO lehrer (schuelerId, klassenId, gueltigAb) VALUES ( $schuelerId, $klassenId, $gueltigAb)',
-    {
-      $schuelerId: req.body.schuelerId,
-      $klassenId: req.body.klassenId,
-      $gueltigAb: req.body.gueltigAb,
-    },
-    (err) => {
-      if (err) {
-        if (err.message.includes('UNIQUE constraint failed')) {
-          res.status(400).json({ message: 'An Item cant be added twice' });
-        } else {
-          res.status(500).json({ message: err.message });
-        }
-      } else {
-        res.status(200).json({ message: 'inserted' });
-      }
-    }
-  );
 });
 
 app.post('/studentInfo', checkToken, (req, res) => {
